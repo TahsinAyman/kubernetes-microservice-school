591d734c3aaf7c743b542ee74e516d9dde1117d1
TahsinAyman <mail4tahsin@gmail.com>
2023-08-25 10:59:29 +0600

    initial commit

--- src/main/java/com/fullstackbd/tahsin/school/client/StudentClient.java
+++ src/main/java/com/fullstackbd/tahsin/school/client/StudentClient.java
@@ -1,10 +1,12 @@
 package com.fullstackbd.tahsin.school.client;
 
 import com.fullstackbd.tahsin.school.entity.Student;
+
+import jakarta.persistence.Entity;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
-
 import java.util.List;
 
 @HttpExchange("/student")
